593c7e112ae2eba0eca0896991ce27cae01be6ad
assoemordemspc-tech <assoemordemspc@gmail.com>
2026-04-15 12:50:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 import { Game } from './src/core/game.js';
 
 window.addEventListener('load', () => {
-    try {
-        const game = new Game();
-        game.start();
-        console.log("Jogo iniciado!");
-    } catch (e) {
-        console.error("Erro ao carregar o jogo:", e);
-    }
+    const game = new Game();
+    game.start();
 });
