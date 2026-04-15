324cdfcd9b1c7f850a998bd144fef20312d17d35
assoemordemspc-tech <assoemordemspc@gmail.com>
2026-04-15 11:36:13 -0300

    Delete MJ directory

--- MJ/main.js
+++
@@ -1,7 +0,0 @@
-import { Game } from './src/core/game.js';
-
-// Aguarda o carregamento do DOM para garantir que o canvas exista
-window.addEventListener('load', () => {
-    const game = new Game();
-    game.start();
-});
