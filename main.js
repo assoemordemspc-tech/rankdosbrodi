36c48155b1863349f81ef8c2df19884b59d8e7b0
assoemordemspc-tech <assoemordemspc@gmail.com>
2026-04-15 12:39:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-import './src/core/teste.js';
 import { Game } from './src/core/game.js';
 
 window.addEventListener('load', () => {
-    try {
-        const game = new Game();
-        game.start();
+    const game = new Game();
+    game.start();
+});
         console.log("Jogo iniciado com sucesso!");
     } catch (error) {
         console.error("Erro ao iniciar o jogo:", error);
