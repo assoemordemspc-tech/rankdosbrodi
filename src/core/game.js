b73a0aa3f1d411a4cfa22b37b0d6428bbdb6d1ea
assoemordemspc-tech <assoemordemspc@gmail.com>
2026-04-15 12:27:16 -0300

    Update game.js

--- src/core/game.js
+++ src/core/game.js
@@ -1,6 +1,6 @@
-import { Player } from '../entities/player.js';
-import { Input } from './input.js';
-import { SpawnSystem } from '../systems/spawnSystem.js';
+import { Player } from '/src/entities/player.js';
+import { Input } from '/src/core/input.js';
+import { SpawnSystem } from '/src/systems/spawnSystem.js';
 
 export class Game {
     constructor() {
