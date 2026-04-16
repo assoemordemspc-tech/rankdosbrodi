9d1df6945e85c42fe816d920c2108e4ec59c60e1
assoemordemspc-tech <assoemordemspc@gmail.com>
2026-04-16 11:37:15 -0300

    Update projectile.js

--- src/entities/projectile.js
+++ src/entities/projectile.js
@@ -1,23 +1,24 @@
 export class Projectile {
-    constructor(x, y, targetX, targetY) {
+    constructor(x, y, angle) {
         this.x = x;
         this.y = y;
-        this.size = 5;
-        this.velocity = 8;
-        const dx = targetX - x;
-        const dy = targetY - y;
-        const dist = Math.hypot(dx, dy);
-        this.vx = (dx / dist) * this.velocity;
-        this.vy = (dy / dist) * this.velocity;
+        this.size = 6;
+        this.velocity = 7;
+        this.damage = 1;
+
+        this.vx = Math.cos(angle) * this.velocity;
+        this.vy = Math.sin(angle) * this.velocity;
     }
+
     update() {
         this.x += this.vx;
         this.y += this.vy;
     }
+
     draw(ctx) {
         ctx.fillStyle = '#ffff00';
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.arc(this.x, this.y, this.size / 2, 0, Math.PI * 2);
         ctx.fill();
     }
 }
