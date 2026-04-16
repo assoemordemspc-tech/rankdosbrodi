c5f2d00c5db19c03124ca317b735748371771735
assoemordemspc-tech <assoemordemspc@gmail.com>
2026-04-16 13:53:57 -0300

    Update projectile.js

--- src/entities/projectile.js
+++ src/entities/projectile.js
@@ -1,42 +1,40 @@
 export class Projectile {
-    constructor(x, y, angle) {
+    constructor(x, y, angle, effect = null) {
         this.x = x;
         this.y = y;
         this.size = 6;
-        this.angle = angle; // Guardamos o ângulo caso precise para efeitos visuais
+        this.angle = angle;
 
         this.speed = 10;
-        this.damage = 1.5; 
+        this.damage = 1.5;
 
-        // 🔥 NOVO: Pierce (Penetração)
-        // O tiro agora atravessa 1 inimigo e desaparece ao atingir o 2º
-        this.pierce = 2; 
+        this.pierce = 2;
 
         this.vx = Math.cos(angle) * this.speed;
         this.vy = Math.sin(angle) * this.speed;
 
-        this.lifeTime = 120; // frames (~2 segundos)
+        this.lifeTime = 120;
+
+        // 🔥 EFFECT LAYER
+        this.effect = effect; // ex: 'burn'
     }
 
     update() {
         this.x += this.vx;
         this.y += this.vy;
-
         this.lifeTime--;
     }
 
-    // O projétil morre se o tempo acabar OU se a penetração chegar a 0
     isDead() {
         return this.lifeTime <= 0 || this.pierce <= 0;
     }
 
     draw(ctx) {
-        ctx.fillStyle = '#ffff00'; // Amarelo brilhante
+        ctx.fillStyle = '#ffff00';
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size / 2, 0, Math.PI * 2);
         ctx.fill();
 
-        // Opcional: Adicionar um brilho simples para parecer um tiro de energia
         ctx.shadowBlur = 10;
         ctx.shadowColor = '#ffff00';
     }
