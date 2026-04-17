20fcc76a5d29bcb669707f6412abcf411f346661
assoemordemspc-tech <assoemordemspc@gmail.com>
2026-04-17 13:23:50 -0300

    Update projectile.js

--- src/entities/projectile.js
+++ src/entities/projectile.js
@@ -2,6 +2,7 @@ export class Projectile {
     constructor(x, y, angle, effect = null) {
         this.x = x;
         this.y = y;
+
         this.size = 6;
         this.angle = angle;
 
@@ -16,12 +17,21 @@ export class Projectile {
         this.lifeTime = 120;
 
         // 🔥 EFFECT LAYER
-        this.effect = effect; // ex: 'burn'
+        this.effect = effect;
+
+        // ✨ NOVO: rastro visual
+        this.history = [];
     }
 
     update() {
+        // ✨ salva histórico (trail)
+        this.history.push({ x: this.x, y: this.y });
+        if (this.history.length > 5) this.history.shift();
+
+        // movimento
         this.x += this.vx;
         this.y += this.vy;
+
         this.lifeTime--;
     }
 
@@ -30,12 +40,25 @@ export class Projectile {
     }
 
     draw(ctx) {
-        ctx.fillStyle = '#ffff00';
+        // ✨ TRAIL (rastro suave)
+        this.history.forEach((pos, index) => {
+            const opacity = index / this.history.length;
+
+            ctx.fillStyle = `rgba(0, 255, 255, ${opacity * 0.5})`;
+            ctx.beginPath();
+            ctx.arc(pos.x, pos.y, (this.size / 2) * opacity, 0, Math.PI * 2);
+            ctx.fill();
+        });
+
+        // 💥 cabeça do projétil
+        ctx.fillStyle = '#ffffff';
+        ctx.shadowBlur = 10;
+        ctx.shadowColor = '#00ffff';
+
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size / 2, 0, Math.PI * 2);
         ctx.fill();
 
-        ctx.shadowBlur = 10;
-        ctx.shadowColor = '#ffff00';
+        ctx.shadowBlur = 0;
     }
 }
